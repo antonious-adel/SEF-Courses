4fab40696f0d4be33b6ad2a825c90941f387cfc7
antonious-adel <154696368+antonious-adel@users.noreply.github.com>
2024-08-29 21:26:06 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Courses from "./components/Courses";
 import Tech from "./components/Tech";
 
 const App = () => {
+    
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<h2> this is main page </h2>} />
-                <Route path="/courses" element={<Courses />} />
-                <Route path="/tech" element={<Tech />} />
-                <Route path="*" element={<h2> page not found </h2>} />
+                
+                < Route path="/" element={<h2> this is main page </h2>} />
+                < Route path="/courses" element={<Courses />} />
+                < Route path="/tech" element={<Tech />} />
+                < Route path="*" element={<h2> page not found </h2>} />
             </Routes>
         </Router>
     )
